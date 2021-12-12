51cbb101939377b2d4aeec33d9e2c83357d7c20a
Spirit-Supreme <achantass25@gmail.com>
2021-12-11 23:06:29 -0500

    Added NotifyUser() fucntion to MainScreen.js
    Passes in Method (Valid Entries are 'vibrate', 'notification'), Location (Valid Entries are 'main room', 'photo room', 'library', 'woodshop') and Status (Valid Entries are 'in' and 'out')

--- app/screens/MainScreen.js
+++ app/screens/MainScreen.js
@@ -6,12 +6,48 @@ import {
     StyleSheet,
     Button,
     ImageBackground,
+    Vibration,
 }
 from 'react-native';
 
-const image = { uri: "https://cdn.discordapp.com/attachments/892936500747075624/919297354170003466/unknown.png" };
+function NotifyUser(Method, Location, Status) { //figure out notifications
+    const d = new Date()
+    var datetime = d.toLocaleString('en-GB')
+    const vibratepattern = [500, 500, 500]; //Three short 1/2 vibrations
+    const locations = [
+        'main room', //Room 31
+        'photo room', //Room 32
+        'library', //Library
+        'woodshop', //Room 33
+    ]
+    if(!Location) {
+        var place = 'null'
+    } else {
+        if(locations.includes(location)) {
+            var place = string(location)
+        } else {
+            var place = 'null'
+        }
+    }
+    //Notify User of Sucsessful Signin
+    switch(string(Method)) {
+        case 'vibrate': //simple buzz
+            Vibration.vibrate(vibratepattern)
+        break;
+        case 'notification': // You signed in at the _ at _
+            if(status == 'in') {
+            console.log("You signed in on "+datetime+" at "+place)
+            } else if(status == 'out') {
+                console.log("You signed out on "+datetime)
+            }
+            //Test but not with push notifications yet
+        break;
+
+    }
+}
 
 const MainScreen = ({ navigation, route }) => {
+    const image = { uri: "https://cdn.discordapp.com/attachments/892936500747075624/919297354170003466/unknown.png" };
     return (
         <View style={styles.container}>
             <ImageBackground source={image} resizeMode="cover" style={styles.image}>
@@ -20,7 +56,7 @@ const MainScreen = ({ navigation, route }) => {
     );
 }
 
-const styles = StyleSheet.create({
+const styles = StyleSheet.create({ //styles
     mainPage: {
         justifyContent: 'center',
         alignItems: 'center',
