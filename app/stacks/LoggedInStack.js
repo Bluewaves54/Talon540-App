7e14c8d5ff86d6b8005674aefb5463c18b2470e0
Spirit-Supreme <achantass25@gmail.com>
2021-12-12 05:50:21 -0500

    Added Icons to LoggedInStack.js + etc
    Updated .xcodemangement for future images and updates. It is 6 am im gonna go sleep.

--- app/stacks/LoggedInStack.js
+++ app/stacks/LoggedInStack.js
@@ -1,36 +1,42 @@
 import React from 'react'
+import { Text, View } from 'react-native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { NavigationContainer } from '@react-navigation/native'
 import MainScreen from '../screens/MainScreen';
 import ProfileStack from './ProfileStack';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 
 const Tab = createBottomTabNavigator();
 
-const LoggedInStack = () => {
-    return ( // Change independent if >NavigationContainers
-            <Tab.Navigator
-                screenOptions={({ route }) => ({
-                    headerShown: false,
+export default function App() {
+  return (
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName;
+            if (route.name === 'Home') {
+              iconName = focused
+                ? 'ios-home'
+                : 'ios-home-outline';
+            } else if (route.name === 'Settings') {
+              iconName = focused 
+                ? 'ios-settings' 
+                : 'ios-settings-outline';
+            }
+            return <Ionicons name={iconName} size={size} color={color} />;
+          },
+          headerShown: false,
                     tabBarStyle: {
                         backgroundColor: '#1f2129'
                     },
-                    tabBarActiveTintColor: 'lightblue',
-                })}
-            >
-                <Tab.Screen
-                    name="Main Page"
-                    component={MainScreen}
-                />
-                <Tab.Screen
-                    name="User Information"
-                    component={ProfileStack}
-                    options={{
-                        headerShown: false,
-                    }}
-                />
-            </Tab.Navigator>
-    );
-  }
+          tabBarActiveTintColor: 'lightblue',
+        })}>
 
 
-export default LoggedInStack
+
+
+        <Tab.Screen name="Home" component={MainScreen} />
+        <Tab.Screen name="Settings" component={ProfileStack} />
+      </Tab.Navigator>
+  );
+}
