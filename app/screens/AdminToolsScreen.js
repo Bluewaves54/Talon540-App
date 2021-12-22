3bcfc87030f0efc2dcdd80d6853dd02f41a42ea6
Spirit-Supreme <68172138+Spirit-Supreme@users.noreply.github.com>
2021-12-21 19:50:50 -0500

    Update AdminToolsScreen.js

--- app/screens/AdminToolsScreen.js
+++ app/screens/AdminToolsScreen.js
@@ -23,7 +23,10 @@ export default function AdminTools() {
           headerTitleStyle: {
             color: 'white',
             fontWeight: 'bold',
-        },
+          },
+          headerStyle: {
+            backgroundColor: '#1f2129',
+          },
           drawerInactiveTintColor: 'white',
           drawerActiveTintColor: globalColor,
           labelStyle:{
